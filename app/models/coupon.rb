8161a194fb2d3f05e1d9cfa0595ca0d68e0c804c
Steven Stackhouse <stackmm@gmail.com>
2023-06-09 11:47:45 -0600

    add method to calculcate discount

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -9,4 +9,12 @@ class Coupon < ApplicationRecord
   has_many :invoices
 
   enum status: [:disabled, :enabled]
+
+  def calculate_discount(price)
+    if value <= 1.0 # a value <= 1.0 respresents a percentage off (max discount = 100%)
+      (price * value).round(2)
+    else
+      [price, value].min.round(2)
+    end
+  end
 end
