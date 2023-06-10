0f3ccd0ef2af2cdc728fd8b6bba7be8e47113f29
Steven Stackhouse <stackmm@gmail.com>
2023-06-10 17:08:05 -0600

    add total revenue with discount and coupon details

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -7,7 +7,9 @@ describe "Admin Invoices Index Page" do
     @c1 = Customer.create!(first_name: "Yo", last_name: "Yoz", address: "123 Heyyo", city: "Whoville", state: "CO", zip: 12345)
     @c2 = Customer.create!(first_name: "Hey", last_name: "Heyz")
 
-    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: "2012-03-25 09:54:09")
+    @coupon_1 = Coupon.create!(name: "Half Off", code: "HALFOFF", value: 0.5, merchant_id: @m1.id)
+
+    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: "2012-03-25 09:54:09", coupon_id: @coupon_1.id)
     @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: "2012-03-25 09:30:09")
 
     @item_1 = Item.create!(name: "test", description: "lalala", unit_price: 6, merchant_id: @m1.id)
@@ -54,7 +56,7 @@ describe "Admin Invoices Index Page" do
   end
 
   it "should display the total revenue the invoice will generate" do
-    expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
+    expect(page).to have_content("Subtotal: $#{@i1.total_revenue}")
 
     expect(page).to_not have_content(@i2.total_revenue)
   end
@@ -68,5 +70,15 @@ describe "Admin Invoices Index Page" do
       expect(current_path).to eq(admin_invoice_path(@i1))
       expect(@i1.status).to eq("completed")
     end
+  end
+
+  it "displays the name and code of any coupon used on an invoice" do
+    expect(page).to have_content(@coupon_1.name)
+    expect(page).to have_content(@coupon_1.code)
+  end
+
+  it "displays the subtotal and total revenue for an invoice" do
+    expect(page).to have_content(@i1.total_revenue)
+    expect(page).to have_content(@i1.total_revenue_with_discount)
   end
 end
