bf9337f0753dcb115d22fb3bc1b62b2b57bf0727
Steven Stackhouse <stackmm@gmail.com>
2023-06-09 18:55:49 -0600

    show view spec

---
+++ spec/features/coupons/show_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Coupon Show Page", type: :feature do
+  before(:each) do
+    coupon_data
+    visit merchant_coupon_path(@merchant1, @coupon_1)
+  end
+
+  # User Story 3
+  it "displays the coupon's name, code, value and status" do
+    expect(page).to have_content(@coupon_1.name)
+    expect(page).to have_content(@coupon_1.code)
+    expect(page).to have_content(@coupon_1.value)
+    expect(page).to have_content(@coupon_1.status)
+
+    expect(page).to_not have_content(@coupon_2.name)
+  end
+
+  it "displays a count of how many times that coupon has been used" do
+    expect(page).to have_content("Times Used: 0")
+  end
+end
