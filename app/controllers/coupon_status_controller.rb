b7bfefc0b46aff990cf62c54fbbf877f7572f2c7
Steven Stackhouse <stackmm@gmail.com>
2023-06-10 20:11:12 -0600

    modify update to have a sad path for too many active coupons

--- app/controllers/coupon_status_controller.rb
+++ app/controllers/coupon_status_controller.rb
@@ -2,8 +2,13 @@ class CouponStatusController < ApplicationController
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @coupon = Coupon.find(params[:id])
-    @coupon.update(coupon_status_params)
-    redirect_to merchant_coupon_path(@merchant, @coupon)
+
+    if @merchant.too_many_active_coupons?
+      flash[:notice] = "You cannot have more than 5 active coupons at a time."
+    else
+      @coupon.update(coupon_status_params)
+      redirect_to merchant_coupon_path(@merchant, @coupon)
+    end
   end
 
   private 
