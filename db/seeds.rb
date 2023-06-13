55d701a66771b1e8fd3fe60cf68477a2e7404571
Steven Stackhouse <stackmm@gmail.com>
2023-06-12 21:56:32 -0600

    minor change to seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ Rake::Task["csv_load:all"].invoke
 @coupon_2 = Coupon.create!(name: "OMG it's FREE!", code: "FULLOFF", value: 1.0, discount_type: "percent-off", merchant_id: 1)
 @coupon_3 = Coupon.create!(name: "10% Off", code: "TENOFF", value: 0.10, discount_type: "percent-off", merchant_id: 1)
 @coupon_4 = Coupon.create!(name: "Five Dollars Off!", code: "5OFF", value: 5, discount_type: "dollars-off", merchant_id: 1)
-@coupon_5 = Coupon.create!(name: "Two Dollars Off!", code: "2OFF", value: 2, discount_type: "dollars-off", merchant_id: 1)
+@coupon_5 = Coupon.create!(name: "Two Dollars Off!", code: "2OFF", value: 2, discount_type: "dollars-off", merchant_id: 3)
 @coupon_6 = Coupon.create!(name: "Ten Dollars Off!", code: "10OFF", value: 10, discount_type: "dollars-off", merchant_id: 2)
 @coupon_7 = Coupon.create!(name: "50% Off!", code: "50%OFF", value: 0.5, discount_type: "percent-off", merchant_id: 2)
 @coupon_8 = Coupon.create!(name: "Three Bucks Off!", code: "3OFF", value: 3, discount_type: "dollars-off", merchant_id: 2)
